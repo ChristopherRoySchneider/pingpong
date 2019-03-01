d8b2c9047dd2c24185b8434525faf0601c2c220d
44778348+ChristopherRoySchneider@users.noreply.github.com <44778348+ChristopherRoySchneider@users.noreply.github.com>
2019-03-01 14:47:41 -0600

    game updates on game win event

--- public/src/app/gctable/gctable.component.ts
+++ public/src/app/gctable/gctable.component.ts
@@ -1,47 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { HttpService } from '../http.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { HttpService } from "../http.service";
 import { SocketService } from "../socket.service";
-import SVG from 'svg.js';
-import { Match } from '../models/match';
+import SVG from "svg.js";
+import { Match } from "../models/match";
 
 @Component({
-  selector: 'app-gctable',
-  templateUrl: './gctable.component.html',
-  styleUrls: ['./gctable.component.css']
+  selector: "app-gctable",
+  templateUrl: "./gctable.component.html",
+  styleUrls: ["./gctable.component.css"]
 })
 export class GctableComponent implements OnInit {
-
-  constructor(
-    private _http: HttpService,
-    private _socket: SocketService
-    ) { }
+  constructor(private _http: HttpService, private _socket: SocketService) {}
 
   @Input("match") match: Match;
   @Input("gameIndex") gameIndex: any;
 
   scoreTypesArray = [
-    'ace',
-    'backhand',
-    'block',
-    'chop',
-    'drop',
-    'flick',
-    'forehand',
-    'hit',
-    'kill',
-    'lob',
-    'loop',
-    'out',
-    'push',
-    'serve',
-    'smash'
+    "ace",
+    "backhand",
+    "block",
+    "chop",
+    "drop",
+    "flick",
+    "forehand",
+    "hit",
+    "kill",
+    "lob",
+    "loop",
+    "out",
+    "push",
+    "serve",
+    "smash"
   ];
 
-  nonScoreTypesArray = [
-    'New Game',
-    'Let',
-    'Service Change'
-  ]
+  nonScoreTypesArray = ["New Game", "Let", "Service Change"];
 
   draw: any;
   table: any;
@@ -55,37 +47,41 @@ export class GctableComponent implements OnInit {
   errors: [];
 
   newGameEventObj = {
-    scorer: '',
+    scorer: "",
     p1_points_scored: 0,
     p2_points_scored: 0,
-    type: '',
+    type: "",
     x: 0,
     y: 0,
-    eventType: ''
-  }
+    eventType: ""
+  };
 
   ngOnInit() {
     this.makeTable();
     // console.log("gameIndex",this.gameIndex)
-    this.newGameEventObj.p1_points_scored=this.match.games[this.gameIndex]['p1_points_scored']
-    this.newGameEventObj.p2_points_scored=this.match.games[this.gameIndex]['p2_points_scored']
+    this.newGameEventObj.p1_points_scored = this.match.games[this.gameIndex][
+      "p1_points_scored"
+    ];
+    this.newGameEventObj.p2_points_scored = this.match.games[this.gameIndex][
+      "p2_points_scored"
+    ];
   }
 
   makeTable() {
-    this.draw = SVG('drawing').size(640, 356)
+    this.draw = SVG("drawing").size(640, 356);
     this.table = this.draw.rect(640, 356).attr({
-      'fill': '#022b6d',
-      'stroke': '#fff',
-      'stroke-width': 10
+      fill: "#022b6d",
+      stroke: "#fff",
+      "stroke-width": 10
     });
     this.centerLine = this.draw.line([[0, 178], [640, 178]]).stroke({
-      'color': '#fff',
-      'width': 5
-    })
+      color: "#fff",
+      width: 5
+    });
     this.net = this.draw.line([[320, 0], [320, 356]]).stroke({
-      'color': '#fff',
-      'width': 5
-    })
+      color: "#fff",
+      width: 5
+    });
   }
 
   populateGameEvent(event: MouseEvent) {
@@ -97,48 +93,72 @@ export class GctableComponent implements OnInit {
     this.ball = this.draw.circle(10).attr({
       cx: this.newGameEventObj.x,
       cy: this.newGameEventObj.y,
-      fill: '#fff'
+      fill: "#fff"
     });
   }
 
   postAndEmitGameEvent() {
-    this.newGameEventObj.eventType = 'score'
+    this.newGameEventObj.eventType = "score";
     this.gameId = this.getGameId(this.match);
-    if (this.newGameEventObj.x < 320){
+    if (this.newGameEventObj.x < 320) {
       this.newGameEventObj.p2_points_scored++;
-
     } else {
       this.newGameEventObj.p1_points_scored++;
-
     }
-    var updatedGame = this.match.games[this.gameIndex]
+    var updatedGame = this.match.games[this.gameIndex];
     updatedGame.p1_points_scored = this.newGameEventObj.p1_points_scored;
     updatedGame.p2_points_scored = this.newGameEventObj.p2_points_scored;
     this.putGameEvent(this.match._id, this.gameId, this.newGameEventObj);
-    this.putGameData(this.match._id,updatedGame)
+    this.putGameData(this.match._id, updatedGame);
   }
 
-  submitNonScoringEvent(){
-    this.newGameEventObj.eventType = 'non-score'
-    this.newGameEventObj.x = null
-    this.newGameEventObj.y = null
-    this.newGameEventObj.scorer = null
-    if (this.newGameEventObj.type == "New Game"){
-      this.addGame(this.match['_id']);
+  submitNonScoringEvent() {
+    this.newGameEventObj.eventType = "non-score";
+    this.newGameEventObj.x = null;
+    this.newGameEventObj.y = null;
+    this.newGameEventObj.scorer = null;
+    if (this.newGameEventObj.type == "New Game") {
+      this.addGame(this.match["_id"]);
     }
-    this.putGameEvent(this.match._id, this.gameId, this.newGameEventObj)
+    var updatedGame = this.match.games[this.gameIndex];
+    this.putGameEvent(this.match._id, this.gameId, this.newGameEventObj);
+    if (this.newGameEventObj.type == "P1 Wins Game") {
+      updatedGame.winner = "p1";
+      updatedGame.game_complete = true;
+
+      this.putGameData(this.match._id, updatedGame);
+      this.match.p1_games_won +=1;
+      this.putMatch(this.match)
+    }
+    if (this.newGameEventObj.type == "P2 Wins Game") {
+      updatedGame.winner = "p2";
+      updatedGame.game_complete = true;
+      this.putGameData(this.match._id, updatedGame);
+      this.match.p2_games_won +=1;
+      this.putMatch(this.match)
+    }
+    if (this.newGameEventObj.type == "P1 Wins Match") {
+      this.match.winner = "p1";
+      this.match.match_complete=true;
+      this.putMatch(this.match)
+    }
+    if (this.newGameEventObj.type == "P2 Wins Match") {
+      this.match.winner = "p2";
+      this.match.match_complete=true;
+      this.putMatch(this.match)
+    }
   }
 
   addGame(matchId) {
-    let observable = this._http.addGame(matchId,{});
+    let observable = this._http.addGame(matchId, {});
     observable.subscribe(data => {
-      console.log('posted data', data);
-      if (data['message'] == 'Error') {
-        console.log('Error saving Match');
-        this.errors = data['error'];
+      console.log("posted data", data);
+      if (data["message"] == "Error") {
+        console.log("Error saving Match");
+        this.errors = data["error"];
         console.log(this.errors);
       } else {
-        this.getMatchByIdFromService(this.match['_id']);
+        this.getMatchByIdFromService(this.match["_id"]);
         this.errors = null;
       }
     });
@@ -146,14 +166,14 @@ export class GctableComponent implements OnInit {
   getMatchByIdFromService(id?: string) {
     let observable = this._http.getMatchById(id);
     observable.subscribe(data => {
-      console.log('Got our match by id the new way!', data);
-      this.match = data['data'][0];
-      console.log('this.matchToEdit', this.match);
+      console.log("Got our match by id the new way!", data);
+      this.match = data["data"][0];
+      console.log("this.matchToEdit", this.match);
     });
   }
 
   determineScorer(x: number): string {
-    if (x < 320){
+    if (x < 320) {
       // this.newGameEventObj.p2_points_scored++;
       return this.match.player2;
     } else {
@@ -163,12 +183,12 @@ export class GctableComponent implements OnInit {
   }
 
   getGameId(match: Match) {
-    return match.games[match.games.length-1]._id;
+    return match.games[match.games.length - 1]._id;
   }
 
   putGameEvent(matchId, gameId, newGameEvent) {
-    newGameEvent.createdAt= new Date().toISOString();
-    newGameEvent.updatedAt= new Date().toISOString();
+    newGameEvent.createdAt = new Date().toISOString();
+    newGameEvent.updatedAt = new Date().toISOString();
     // console.log("Event emitted. Sending:", newGameEvent);
 
     var gameEventdata = {
@@ -177,11 +197,7 @@ export class GctableComponent implements OnInit {
       gameid: gameId
     };
 
-    this._http.postGameEvent(
-      matchId,
-      gameId,
-      newGameEvent
-    ).subscribe(data => {
+    this._http.postGameEvent(matchId, gameId, newGameEvent).subscribe(data => {
       // console.log("put game event", data);
 
       if (data["message"] == "Error") {
@@ -192,19 +208,14 @@ export class GctableComponent implements OnInit {
     });
   }
   putGameData(matchId, updatedGame) {
-
     console.log("sending updated game data:", updatedGame);
 
     var gameData = {
       updatedGame: updatedGame,
-      matchid: matchId,
-
+      matchid: matchId
     };
 
-    this._http.putGame(
-      matchId,
-      updatedGame,
-    ).subscribe(data => {
+    this._http.putGame(matchId, updatedGame).subscribe(data => {
       console.log("put game :", data);
 
       if (data["message"] == "Error") {
@@ -214,4 +225,22 @@ export class GctableComponent implements OnInit {
       }
     });
   }
-}
+  putMatch(updatedMatch) {
+
+    console.log(updatedMatch);
+    let observable = this._http.putMatch(updatedMatch);
+    observable.subscribe(data => {
+      console.log("put match", data);
+
+      if (data['message'] == 'Error') {
+        console.log('Error saving Match');
+        this.errors = data['error'];
+        console.log(this.errors);
+      } else {
+
+        this._socket.sendMatchUpdate(updatedMatch);
+
+        this.errors = null;
+      }
+    });
+}}
